3dea3585fe11c246e3b32ba073a995e538e8c82a
Mrunal Nirajkumar Shah <mrunalnshah@protonmail.com>
2024-08-02 09:13:35 +0530

    [ 0027 ] LeetCode { Added }
    [ 0027 ] LeetCode { Added }
          Remove Element
          [ Static Array ]

---
+++ LeetCode/Problems/0027. Remove_Element/01.cpp
@@ -0,0 +1,45 @@
+/*
+  Problem Number : 0027
+  Problem Title  : Remove Elements
+  Link :
+    https://leetcode.com/problems/remove-element/description/
+
+  Method : Creating a new Vector and storing the values which are not the key.
+
+  Author : Mrunal Nirajkumar Shah
+  Date   : 2st of August, 2024
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  int removeElement(vector<int>& nums, int val) {
+    vector<int> temp;
+    for (int i = 0; i < nums.size(); i++) {
+      if (nums[i] == val) {
+        continue;
+      } else {
+        temp.push_back(nums[i]);
+      }
+    }
+    nums = temp;
+    return nums.size();
+  }
+};
+
+int main() {
+  Solution mySolution;
+  //  vector<int> nums = {0, 1, 2, 2, 3, 0, 4, 2};
+  //  int val = 2;
+
+  vector<int> nums = {3, 2, 2, 3};
+  int val = 3;
+
+  cout << mySolution.removeElement(nums, val) << endl;
+
+  return 0;
+}
---
+++ LeetCode/Problems/0027. Remove_Element/02.cpp
@@ -0,0 +1,55 @@
+/*
+  Problem Number : 0027
+  Problem Title  : Remove Elements
+  Link :
+    https://leetcode.com/problems/remove-element/description/
+
+  Method : Using the nums vector and removing elements(val) from it. {no new
+  vector created}
+
+  Author : Mrunal Nirajkumar Shah
+  Date   : 2st of August, 2024
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  void removeElementFromVector(vector<int>& nums, int index) {
+    for (int i = index; i < nums.size() - 1; i++) {
+      nums[i] = nums[i + 1];
+    }
+    nums.pop_back();
+  }
+  int removeElement(vector<int>& nums, int val) {
+    if (nums.empty()) {
+      return 0;
+    }
+
+    int i = 0;
+    while (i < nums.size()) {
+      if (nums[i] == val) {
+        removeElementFromVector(nums, i);
+      } else {
+        i++;
+      }
+    }
+    return nums.size();
+  }
+};
+
+int main() {
+  Solution mySolution;
+  vector<int> nums = {0, 1, 2, 2, 3, 0, 4, 2};
+  int val = 2;
+
+  // vector<int> nums = {3, 2, 2, 3};
+  // int val = 3;
+
+  cout << mySolution.removeElement(nums, val) << endl;
+
+  return 0;
+}
